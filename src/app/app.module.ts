debf49f00e513402c59db021c5e3ade490158091
khushangdhingra <khushangd@gmail.com>
2022-08-02 14:59:19 +0530

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputFormComponent } from './input-form/input-form.component';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import {CalendarModule} from 'primeng/calendar';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {ChipsModule} from 'primeng/chips';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AutoCompleteModule,
+    CalendarModule,
+    InputNumberModule,
+    ChipsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/input-form/input-form.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+
+@Component({
+  selector: 'app-input-form',
+  templateUrl: './input-form.component.html',
+  styleUrls: ['./input-form.component.css']
+})
+export class InputFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.es = {
+      firstDayOfWeek: 1,
+      dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
+      dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
+      dayNamesMin: [ "D","L","M","X","J","V","S" ],
+      monthNames: [ "enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre" ],
+      monthNamesShort: [ "ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic" ],
+      today: 'Hoy',
+      clear: 'Borrar'
+  }
+
+  let today = new Date();
+  let month = today.getMonth();
+  let year = today.getFullYear();
+  let prevMonth = (month === 0) ? 11 : month -1;
+  let prevYear = (prevMonth === 11) ? year - 1 : year;
+  let nextMonth = (month === 11) ? 0 : month + 1;
+  let nextYear = (nextMonth === 0) ? year + 1 : year;
+  this.minDate = new Date();
+  this.minDate.setMonth(prevMonth);
+  this.minDate.setFullYear(prevYear);
+  this.maxDate = new Date();
+  this.maxDate.setMonth(nextMonth);
+  this.maxDate.setFullYear(nextYear);
+
+  let invalidDate = new Date();
+  invalidDate.setDate(today.getDate() - 1);
+  this.invalidDates = [today,invalidDate];
+
+  }
+
+  disabled: boolean = true;
+
+  value1!: string;
+
+  value2!: string;
+
+  value3!: string;
+
+  value4!: string;
+
+  value5: string = 'Disabled';
+
+  date3!: Date;
+
+  es: any;
+
+  value17!: number ;
+
+  values1!: string[];
+
+
+  invalidDates!: Array<Date>;
+  dates!: Date[];
+
+    rangeDates!: Date[];
+
+    minDate!: Date;
+
+    maxDate!: Date;
+
+
+
+}
